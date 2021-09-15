612a180ae7c4f56fb0c04a8c373c16cef4f19f2f
Will Smythe <will.smythe@gmail.com>
2021-09-15 11:15:26 -0400

    Update lib.js

--- test/lib.js
+++ test/lib.js
@@ -1,10 +1,9 @@
-import { strictEqual } from 'assert';
 import { calculate } from '../lib.js'
 
 describe('lib', () => {
   describe('#calculate', () => {
-    it('two numbers', function() {
-      strictEqual(calculate(1, 1), 2);      
+    it('two numbers', () => {
+      return calculate(1, 1) == 2;
     });
   });
 });
